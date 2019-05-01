4b306157e5396e44e0e3d636807574524ee396eb
James Shieh <jamesshieh0510@gmail.com>
2019-05-02 03:13:13 +0800

    fix complement sign bug

--- jwt-test.py
+++ jwt-test.py
@@ -4,15 +4,16 @@ import hashlib
 import binascii
 
 header	='{"alg":"HS256","typ":"JWT"}'
-payload = '{"loggedInAs":"admin","iat":1422779638}'
-key 	= "secretkey"
-
+payload = '{"user":"jamesshieh1111","password":"1234567"}'
+key 	= "fji234j;raewr823423"
 #convert utf-8 string to byte format
 def toBytes(string):
 	return bytes(string,'utf-8')
 
 def encodeBase64(text):
-	return base64.urlsafe_b64encode(text)
+	#remove "=" sign, 
+	#P.S. "=" sign is used only as a complement in the final process of encoding a message. 
+	return base64.urlsafe_b64encode(text).replace(b'=',b'')
 
 #signature = HMAC-SHA256(key, unsignedToken)
 def create_sha256_signature(key, unsignedToken):
@@ -20,10 +21,10 @@ def create_sha256_signature(key, unsignedToken):
 	return encodeBase64(signature)
 
 unsignedToken 	=encodeBase64(toBytes(header)) + toBytes('.') + encodeBase64(toBytes(payload))
+#unsignedToken	=toBytes(unsignedToken.decode("utf-8").replace("=",''))
 signature 		=create_sha256_signature(key,unsignedToken)
 
-#remove "=" sign, 
-#P.S. "=" sign is used only as a complement in the final process of encoding a message. 
-jwt_toekn=unsignedToken.decode("utf-8") +'.'+signature.decode("utf-8").replace("=",'')
+
+jwt_toekn=unsignedToken.decode("utf-8") +'.'+signature.decode("utf-8")
 print(jwt_toekn)
 
